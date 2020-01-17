e3b8abb87a20b02c078fef30cb2539d58a670dea
Adem ilter <ademilter@gmail.com>
2020-01-17 12:16:53 +0300

    ana sayfaya gelecek makale sayısı düzeltildi

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,12 +9,27 @@ type Props = {
 const Layout: NextPage<Props> = ({ children }) => {
   return (
     <div>
-      <div>
+      <div style={{ marginBottom: 20 }}>
         <Link href="/">
           <a>Home</a>
         </Link>
+        -
+        <Link href="/new">
+          <a>New</a>
+        </Link>
       </div>
       <main>{children}</main>
+      <style jsx global>
+        {`
+          * {
+            margin: 0;
+            padding: 0;
+          }
+          ul {
+            list-style: none;
+          }
+        `}
+      </style>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Link from 'next/link'
 import fetch from 'isomorphic-unfetch'
+import { formatDistance } from 'date-fns'
+import { tr } from 'date-fns/locale'
 import { NextPage } from 'next'
 import { Article, Tag } from '../interfaces'
 import Layout from '../components/layout'
@@ -15,19 +17,23 @@ const IndexPage: NextPage<Props> = ({ articles, tags }) => {
   return (
     <Layout>
       {articles.map(article => (
-        <div key={article.id}>
-          <div>
+        <div className="article" key={article.id}>
+          <h3>
             <Link href={`article/${article.slug}`}>
               <a>{article.title}</a>
             </Link>
-          </div>
-          <ul>
+          </h3>
+          <ul className="meta">
             <li>
               <Link href={`user/${article.user.username}`}>
                 <a>{article.user.username}</a>
               </Link>
             </li>
-            <li>{article.created_at}</li>
+            <li>
+              {formatDistance(new Date(article.created_at), new Date(), {
+                locale: tr
+              })}
+            </li>
             <li>
               {article.tags.map(tag => (
                 <Link key={tag.id} href={`tag/${tag.slug}`}>
@@ -36,16 +42,33 @@ const IndexPage: NextPage<Props> = ({ articles, tags }) => {
               ))}
             </li>
           </ul>
-          <hr />
         </div>
       ))}
+      <style jsx>
+        {`
+          .article {
+            padding: 20px;
+            border-bottom: 1px solid #ddd;
+          }
+          .meta {
+            display: flex;
+          }
+          .meta li {
+            margin-right: 10px;
+          }
+        `}
+      </style>
     </Layout>
   )
 }
 
 IndexPage.getInitialProps = async () => {
-  const _articles = fetch(`${process.env.API_URL}/articles`)
-  const _tags = fetch(`${process.env.API_URL}/tags`)
+  const _articles = fetch(
+    `${process.env.API_URL}/articles?_start=0&_limit=10&_sort=created_at:DESC`
+  )
+  const _tags = fetch(
+    `${process.env.API_URL}/tags?_start=0&_limit=10&_sort=created_at:DESC`
+  )
   const [resArticles, resTags] = await Promise.all([_articles, _tags])
   const articles: Article[] = await resArticles.json()
   const tags: Tag[] = await resTags.json()
