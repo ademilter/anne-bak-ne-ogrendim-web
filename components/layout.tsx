0b8e9186dabbed94f3bf2fb09d4f060636bbcc36
Adem ilter <ademilter@gmail.com>
2020-02-06 01:43:26 +0300

    refactor

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import { useRouter } from 'next/router'
 import { NextPage } from 'next'
 import Link from 'next/link'
@@ -12,6 +12,7 @@ const Layout: NextPage<Props> = ({ children }) => {
 
   return (
     <div className="app">
+      {/* header */}
       <header className="header">
         <Link href="/">
           <a className="logo">Anne Bak Ne Öğrendim</a>
@@ -24,74 +25,9 @@ const Layout: NextPage<Props> = ({ children }) => {
           </nav>
         )}
       </header>
+
+      {/* content */}
       <main>{children}</main>
-      <style jsx global>
-        {`
-          * {
-            box-sizing: border-box;
-          }
-          html {
-            font: 16px / normal -apple-system, BlinkMacSystemFont, Segoe UI,
-              Roboto, Helvetica Neue, Arial, sans-serif;
-            line-height: 1.5;
-            color: #ddd;
-            background-color: #222;
-            -webkit-font-smoothing: antialiased;
-          }
-          body {
-            margin: 0;
-            padding: 2rem;
-          }
-          a {
-            color: inherit;
-          }
-          .app {
-            max-width: 36rem;
-            margin-left: auto;
-            margin-right: auto;
-          }
-          .header {
-            display: flex;
-            align-items: baseline;
-            margin-bottom: 2rem;
-          }
-          .nav {
-            margin-left: auto;
-          }
-          .nav a + a {
-            margin-left: 1rem;
-          }
-          .logo {
-            font-weight: bold;
-            font-size: 1.2rem;
-          }
-          .editor {
-            max-width: 760px;
-            margin: 0 auto;
-          }
-          trix-editor {
-            margin-top: 0.5rem;
-            padding: 0;
-            border: 0;
-            min-height: 300px;
-            color: inherit;
-          }
-          .trix-button-group.trix-button-group--file-tools {
-            display: none;
-          }
-          trix-toolbar .trix-button {
-            background: #aaa;
-            color: white;
-          }
-          trix-toolbar .trix-button.trix-active {
-            background: #ccc;
-            color: white;
-          }
-          trix-editor:empty:not(:focus)::before {
-            color: #999;
-          }
-        `}
-      </style>
     </div>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Router from 'next/router'
+
 import * as gtag from '../lib/gtag'
 import Seo from '../components/seo'
+
 import 'trix/dist/trix.css'
 
 Router.events.on('routeChangeComplete', url => gtag.pageview(url))
@@ -11,6 +13,73 @@ function MyApp({ Component, pageProps }) {
     <>
       <Seo />
       <Component {...pageProps} />
+      <style jsx global>
+        {`
+          * {
+            box-sizing: border-box;
+          }
+          html {
+            font: 16px / normal -apple-system, BlinkMacSystemFont, Segoe UI,
+              Roboto, Helvetica Neue, Arial, sans-serif;
+            line-height: 1.5;
+            color: #ddd;
+            background-color: #222;
+            -webkit-font-smoothing: antialiased;
+          }
+          body {
+            margin: 0;
+            padding: 2rem;
+          }
+          a {
+            color: inherit;
+          }
+          .app {
+            max-width: 36rem;
+            margin-left: auto;
+            margin-right: auto;
+          }
+          .header {
+            display: flex;
+            align-items: baseline;
+            margin-bottom: 2rem;
+          }
+          .nav {
+            margin-left: auto;
+          }
+          .nav a + a {
+            margin-left: 1rem;
+          }
+          .logo {
+            font-weight: bold;
+            font-size: 1.2rem;
+          }
+          .editor {
+            max-width: 760px;
+            margin: 0 auto;
+          }
+          trix-editor {
+            margin-top: 0.5rem;
+            padding: 0;
+            border: 0;
+            min-height: 300px;
+            color: inherit;
+          }
+          .trix-button-group.trix-button-group--file-tools {
+            display: none;
+          }
+          trix-toolbar .trix-button {
+            background: #aaa;
+            color: white;
+          }
+          trix-toolbar .trix-button.trix-active {
+            background: #ccc;
+            color: white;
+          }
+          trix-editor:empty:not(:focus)::before {
+            color: #999;
+          }
+        `}
+      </style>
     </>
   )
 }
